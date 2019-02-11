1285cef1b73f6246f8da1bd1d37971019abab87c
Yann 'Ze' Richard <ze@nbox.org>
2019-02-11 23:06:04 +0100

    Add: Configure atoum for coveralls/travisCI
    Signed-off-by: Yann 'Ze' Richard <ze@nbox.org>

--- .atoum.php
+++ .atoum.php
@@ -9,47 +9,37 @@ More information on documentation:
 [fr] http://docs.atoum.org/fr/chapter3.html#Fichier-de-configuration
 */
 
-$report = $script->addDefaultReport();
+use mageekguy\atoum\reports;
 
-/*
-LOGO
-*/
-// This will add the atoum logo before each run.
-// $report->addField(new atoum\report\fields\runner\atoum\logo());
+$runner
+    ->addTestsFromDirectory(__DIR__ . '/tests/units/')
+    ->disallowUsageOfUndefinedMethodInMock();
 
-// This will add a green or red logo after each run depending on its status.
-// $report->addField(new atoum\report\fields\runner\result\logo());
+$runner->getScore()->getCoverage();
 
-/*
-CODE COVERAGE SETUP
-*/
-// Please replace in next line "Project Name" by your project name and "/path/to/destination/directory" by your destination directory path for html files.
-$coverage_field = new atoum\report\fields\runner\coverage\html('php-pwned-passwords', __DIR__.'/reports/atoum');
-
-// Please replace in next line http://url/of/web/site by the root url of your code coverage web site.
-// $coverage_field->setRootUrl('http://url/of/web/site');
-
-$report->addField($coverage_field);
-
-/*
-TEST GENERATOR SETUP
-*/
-$test_generator = new atoum\test\generator();
-
-// Please replace in next line "/path/to/your/tests/units/classes/directory" by your unit test's directory.
-$test_generator->setTestClassesDirectory(__DIR__.'/tests/units/');
-
-// Please replace in next line "your\project\namespace\tests\units" by your unit test's namespace.
-$test_generator->setTestClassNamespace('UniversiteRennes2\PwnedPasswords\tests\units');
-
-// Please replace in next line "/path/to/your/classes/directory" by your classes directory.
-$test_generator->setTestedClassesDirectory(__DIR__.'/src/PwnedPasswords.php');
-
-// Please replace in next line "your\project\namespace" by your project namespace.
-$test_generator->setTestedClassNamespace('UniversiteRennes2\PwnedPasswords');
-
-// Please replace in next line "path/to/your/tests/units/runner.php" by path to your unit test's runner.
-// $test_generator->setRunnerPath('path/to/your/tests/units/runner.php');
-
-$script->getRunner()->setTestGenerator($test_generator);
-
+$travis = getenv('TRAVIS');
+if ($travis)
+{
+    $script->addDefaultReport();
+    $coverallsToken = getenv('COVERALLS_REPO_TOKEN');
+    if ($coverallsToken)
+    {
+        $coverallsReport = new reports\asynchronous\coveralls('classes', $coverallsToken);
+        $defaultFinder = $coverallsReport->getBranchFinder();
+        $coverallsReport
+            ->setBranchFinder(function() use ($defaultFinder) {
+                    if (($branch = getenv('TRAVIS_BRANCH')) === false)
+                    {
+                        $branch = $defaultFinder();
+                    }
+                    return $branch;
+                }
+            )
+            ->setServiceName(getenv('TRAVIS') ? 'travis-ci' : null)
+            ->setServiceJobId(getenv('TRAVIS_JOB_ID') ?: null)
+            ->addDefaultWriter()
+        ;
+        $runner->addReport($coverallsReport);
+        $script->testIt();
+    }
+}
